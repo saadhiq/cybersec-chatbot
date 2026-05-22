4041fc0ffa805fe8f9575271a860da999666a849
saadhiq <hassaan.edu@gmail.com>
2026-05-22 16:22:02 +0530

    in admin and studen html page BASE url updated

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,7 @@ SECRET_KEY = 'django-insecure-^^(881@34w3_s*&xwx^t_t0l)=)q+mn6mxb#p*v#-#yxma23!#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
